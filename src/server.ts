1b77b9094668cf20e225e9fad82e657556e3778a
AleksandrRyb <inkognito19931@gmail.com>
2023-03-02 00:45:43 +0300

    some changes in eslint

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,2 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-
-// Create Express server
-const app = express();
-
-// Express configuration
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
-
-// Logging
-app.use(morgan('combined'));
-
-// Start server
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on port ${process.env.PORT || 3000}`);
-});
+const foo = 'bar';
+console.log(foo);
