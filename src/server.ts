49e544066df13a05bc327d158d044593c3546934
AleksandrRyb <inkognito19931@gmail.com>
2023-03-02 01:20:42 +0300

    add the dotenb and env variables

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import env from './config/env';
+
+const { port } = env;
+
 // Create Express server
 const app = express();
 
@@ -17,7 +21,6 @@ app.use(helmet());
 app.use(morgan('combined'));
 
 // Start server
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on port ${process.env.PORT || 3000}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
