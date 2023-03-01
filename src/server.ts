3eda5dee54ea5701f9cfc8f7e363339d944b6d56
AleksandrRyb <inkognito19931@gmail.com>
2023-03-02 00:46:42 +0300

    Retunr server config

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,23 @@
-const foo = 'bar';
-console.log(foo);
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+// Logging
+app.use(morgan('combined'));
+
+// Start server
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
