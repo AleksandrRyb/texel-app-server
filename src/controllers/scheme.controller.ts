fdb6c16658cc9e41eeff12fb51920c4010c7935d
AleksandrRyb <inkognito19931@gmail.com>
2023-03-16 15:44:49 +0300

    imprved types

--- src/controllers/scheme.controller.ts
+++ src/controllers/scheme.controller.ts
@@ -22,13 +22,15 @@ class SchemeController {
       const scriptArgs = Object.keys(body).map((key) => body[key]);
       const scriptPath = './scripts/start.sh';
 
-      const result = await executeShellScript(scriptPath, scriptArgs);
+      const result = await executeShellScript({ scriptPath, scriptArgs });
 
       if (result.error) {
-        throw new Error(result.error);
+        throw new Error(JSON.stringify(result.error));
       }
 
-      res.status(200).json({ message: removeNewlines(result.message) });
+      res
+        .status(200)
+        .json({ message: removeNewlines(result.message as string) });
       res.end();
     } catch (error) {
       res.status(400).json(error);
--- src/services/execute-shell-script.ts
+++ src/services/execute-shell-script.ts
@@ -1,10 +1,22 @@
-import { exec } from 'child_process';
+import { ExecException, exec } from 'child_process';
+
+type ExecuteShellScript = {
+  scriptPath: string;
+  scriptArgs: string | string[];
+};
+
+type ReturnOfShellScriptExecution = {
+  error?: string | ExecException;
+  message?: string;
+};
 
 export async function executeShellScript(
-  scriptPath: string,
-  scriptArgs: string[]
-): Promise<{ [key: string]: string }> {
-  const scriptCommand = `${scriptPath} ${scriptArgs.join(' ')}`;
+  args: ExecuteShellScript
+): Promise<ReturnOfShellScriptExecution> {
+  const { scriptPath, scriptArgs } = args;
+  const scriptCommand = `${scriptPath} ${
+    typeof scriptArgs === 'string' ? scriptArgs : scriptArgs.join(' ')
+  }`;
 
   return await new Promise<{ [key: string]: string }>((resolve, reject) => {
     exec(scriptCommand, (error, stdout, stderr) => {
